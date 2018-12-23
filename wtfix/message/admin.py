ea01ce14c5a69861f3d61d8645159b67b8d214ae
jcass77 <john.cass77@gmail.com>
2018-12-23 07:07:56 +0200

    Don't try to collect TestRequest as a test case class.

--- wtfix/message/admin.py
+++ wtfix/message/admin.py
@@ -14,6 +14,9 @@ class Heartbeat(OptimizedGenericMessage):
 class TestRequest(OptimizedGenericMessage):
     """Generic TestRequest message"""
 
+    # Let pytest know that it should not try to collect this class as a test case just because it is named Test*
+    __test__ = False
+
     def __init__(self, test_request_id):
         super().__init__(
             (Tag.MsgType, MsgType.TestRequest), (Tag.TestReqID, test_request_id)
