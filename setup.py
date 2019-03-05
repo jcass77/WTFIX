5ca387534e697b4b3d586b6126bc0fddc23aefe7
jcass77 <john.cass77@gmail.com>
2019-03-05 15:20:02 +0200

    chore(build): Update package dependencies.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     ],
     keywords="FIX financial information exchange",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    install_requires=["python-dotenv~=0.10", "unsync~=1.1", "aiofiles~=0.4"],
+    install_requires=["python-dotenv~=0.10", "unsync~=1.1"],
     python_requires=">=3.6",
     project_urls={
         "Bug Reports": "https://github.com/jcass77/WTFIX/issues",
